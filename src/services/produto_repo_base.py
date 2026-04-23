67438ed5f4d9ad3deebd9850c0345b61c2759cca
Jovandosg <jovando@gmail.com>
2026-04-23 15:52:28 -0300

    Atualizacao_produto_repo_base

--- src/services/produto_repo_base.py
+++ src/services/produto_repo_base.py
@@ -10,4 +10,21 @@ class ProdutoRepositoryBase(ABC):
     """" Contrato para implementação de produtos"""
 
     @abstractmethods
-    def listar(self) -> List
+    def listar(self) -> List[Produto]:
+        raise NotImplementedError
+    
+    @abstractmethods
+    def obter_por_id(self, id: int) -> Optional[Produto]:
+        raise NotImplementedError
+    
+    @abstractmethods
+    def criar(self, nome: str, preco: float, ativo: bool) -> Produto:
+        raise NotImplementedError
+    
+    @abstractmethods
+    def atualizar(self, id: int, nome: str, preco: float, ativo: bool) -> Optional[Produto]:
+        raise NotImplementedError
+    
+    @abstractmethods
+    def deletar(self, id: int) -> bool:
+        raise NotImplementedError
